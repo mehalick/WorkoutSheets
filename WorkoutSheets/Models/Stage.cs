34d132f3fecad5aebb481dbf39effb83896ce367
Andy Mehalick <andy.mehalick@orangejetpack.com>
2018-05-18 11:50:39 +0300

    initial commit

---
+++ WorkoutSheets/Models/Stage.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkoutSheets.Models
+{
+    public class Stage
+    {
+        public string Title { get; set; }
+        public int Weeks { get; set; } = 3;
+        public int DaysPerWeek { get; set; } = 3;
+        public int[] Sets { get; set; }
+        public int[] Repeats { get; set; }
+        public string[] Exercises { get; set; }
+    }
+}
---
+++ WorkoutSheets/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WorkoutSheets.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
