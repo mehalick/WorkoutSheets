291c741a4bcb1664d2f70a3cc15d3ea0589e510d
Andy Mehalick <andy.mehalick@orangejetpack.com>
2018-06-01 12:37:46 +0300

    make worksheet list dynamic

--- WorkoutSheets/Pages/Index.cshtml.cs
+++ WorkoutSheets/Pages/Index.cshtml.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WorkoutSheets.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public IEnumerable<string> Files { get; private set; }
+
+        public IndexModel(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
         public void OnGet()
         {
+            var path = Path.Combine(_hostingEnvironment.ContentRootPath, @"Pages");
+
+            Files = Directory.EnumerateFiles(path, "stage-*.cshtml").Select(Path.GetFileNameWithoutExtension);
         }
     }
 }
